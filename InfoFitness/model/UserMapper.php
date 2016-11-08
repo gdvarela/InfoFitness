11434415af25dfd12f06ea028ac591e25050a32f
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-08 19:02:58 +0100

    funciones añadidas

--- InfoFitness/model/UserMapper.php
+++ InfoFitness/model/UserMapper.php
@@ -11,31 +11,50 @@ class UserMapper {
     $this->db = PDOConnection::getInstance();
   }
 
-/*  public function save($user) {
-    $stmt = $this->db->prepare("INSERT INTO usuario values (?,?)");
-    $stmt->execute(array($user->getUsername(), $user->getPasswd()));
-  }*/
+
+  //añadir/almacenar usuario en la BD
   public function save($user) {
-    //$name=NULL,$firstname=NULL, $dni=NULL, $fechanac=NULL, $email=NULL, $telef=NULL
+
     $stmt = $this->db->prepare("INSERT INTO usuario values (?,?,?,?,?,?,?,?,?)");
-    $stmt->execute(array($user->getUsername(), $user->getDni(), $user->getNombre(), $user->getApellidos(), $user->getEmail(), $user->getPasswd(), $user->getPermiso(), $user->getTelefono(), $user->getFechanac()));
+    $stmt->execute(array($user->getUsername(), $user->getDni(), $user->getNombre(), $user->getApellidos(), $user->getEmail(),
+     $user->getPasswd(), $user->getPermiso(), $user->getTelefono(), $user->getFechanac()));
   }
 
+  //Usuario existente??
   public function usernameExists($username) {
     $stmt = $this->db->prepare("SELECT count(id_usuario) FROM usuario where id_usuario=?");
     $stmt->execute(array($username));
 
     if ($stmt->fetchColumn() > 0) {
       return true;
+    }else{
+      return false;
     }
   }
 
+  //usuario y contraseña valido????
   public function isValidUser($username, $passwd) {
     $stmt = $this->db->prepare("SELECT count(id_usuario) FROM usuario where id_usuario=? and contraseña=?");
     $stmt->execute(array($username, $passwd));
 
     if ($stmt->fetchColumn() > 0) {
       return true;
+    }else{
+      return false;
     }
+  }
+
+  //actualizar/modificar usuario en la BD
+  public function update($user) {
+      $stmt = $this->db->prepare("UPDATE usuario set id_usuario=?, nombre=?, apellidos=?, mail=?, contraseña=?,
+          permisos=?, telefono=?, fecha_nacimiento=?  where dni=?");
+      $stmt->execute(array($user->getUsername(), $user->getNombre(), $user->getApellidos(),
+          $user->getEmail(), $user->getPasswd(), $user->getPermiso(), $user->getTelefono(), $user->getFechanac(), $user->getDni()));
+  }
+
+  //eliminar usuario de la BD
+  public function delete($username) {
+      $stmt = $this->db->prepare("DELETE FROM usuario WHERE id_usuario=?");
+      $stmt->execute(array($username));
   }
 }
