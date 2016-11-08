cfbe85fd431306dab87b9b29695c1b2d2e9712f2
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-08 19:02:31 +0100

    html añadido

--- InfoFitness/view/users/alta.php
+++ InfoFitness/view/users/alta.php
@@ -5,17 +5,19 @@ $view = ViewManager::getInstance();
 
 
 ?>
+<!DOCTYPE html>
+<html>
+  <form action="index.php?controller=users&amp;action=alta" method="POST">
+      <?= i18n("Username")?>: <input type="text" name="username" value=""><br>
+      <?= i18n("Password")?>: <input type="password" name="passwd" value=""><br>
+      <?= i18n("Name")?>: <input type="text" name="nombre" value=""><br>
+      <?= i18n("Surnames")?>: <input type="text" name="apellidos" value=""><br>
+      <?= i18n("ID")?>: <input type="text" name="dni" value=""><br>
+      <?= i18n("Birthdate")?>: <input type="date" name="fechanac" value=""><br>
+      <?= i18n("Email")?>: <input type="email" name="email" value=""><br>
+      <?= i18n("Phone number")?>: <input type="text" name="telef" value=""><br>
+      <?= i18n("Permissions")?>: <input type="number" name="permiso" value=""><br>
 
-<form action="index.php?controller=users&amp;action=alta" method="POST">
-    <?= i18n("Username")?>: <input type="text" name="username" value=""><br>
-    <?= i18n("Password")?>: <input type="password" name="passwd" value=""><br>
-    <?= i18n("Name")?>: <input type="text" name="nombre" value=""><br>
-    <?= i18n("Surnames")?>: <input type="text" name="apellidos" value=""><br>
-    <?= i18n("ID")?>: <input type="text" name="dni" value=""><br>
-    <?= i18n("Birthdate")?>: <input type="date" name="fechanac" value=""><br>
-    <?= i18n("Email")?>: <input type="email" name="email" value=""><br>
-    <?= i18n("Phone number")?>: <input type="text" name="telef" value=""><br>
-    <?= i18n("Permissions")?>: <input type="number" name="permiso" value=""><br>
-
-    <input type="submit" value="<?= i18n("Register")?>">
-</form>
+      <input type="submit" value="<?= i18n("Register")?>">
+  </form>
+</html>
