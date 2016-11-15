6b6fde193ecbb15a0aea386ce616313ef60f8cd8
gdvarela <guillermo.davilavarela@gmail.com>
2016-11-15 17:10:47 +0100

    Fix BaseController

--- InfoFitness/controller/BaseController.php
+++ InfoFitness/controller/BaseController.php
@@ -30,7 +30,7 @@ class BaseController
             //add current user to the view, since some views require it
             $this->view->setVariable("currentusername", $this->currentUser->getUsername());
         } else {
-            $this->view->redirect("users", "login");
+            $this->view->render("users", "login");
         }
     }
 }
