fcdac196fc32cdc6d2dbfc7f15e4fd26f8e28b0d
gdvarela <guillermo.davilavarela@gmail.com>
2016-11-15 16:43:02 +0100

    Monitor view added Assistance control

--- InfoFitness/view/index/monitor.php
+++ InfoFitness/view/index/monitor.php
@@ -6,3 +6,4 @@ $view = ViewManager::getInstance();
 
 <a href="?controller=exercises&action=listExercises"><?= i18n("Exercises Management") ?></a></br>
 <a href="?controller=tables&action=listtables"><?= i18n("Tables Management") ?></a>
+<a href="?controller=activities&action=assistance"><?= i18n("Assistance Control") ?></a>
