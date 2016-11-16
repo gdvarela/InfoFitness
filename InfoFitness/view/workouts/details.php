d8b3fc48d6358c049485a41d32c6e8a3f36f0887
lrcortizo <luisraco95@gmail.com>
2016-11-16 13:24:22 +0100

    session controller creado y enlazado(incompleto)

---
+++ InfoFitness/view/workouts/details.php
@@ -0,0 +1,40 @@
+<?php
+require_once(__DIR__."/../../core/ViewManager.php");
+$view = ViewManager::getInstance();
+
+if($_SESSION["type"] != 0) {
+    $view->redirect("index", "unauthorized");
+}
+
+$tables = $view->getVariable("tables");
+$exercisesTable = $view->getVariable("exercisesTable");
+$exercises = $view->getVariable("exercises");
+$exercisesNotAsigned = $view->getVariable("exercisesNotAsigned");
+$newTable = $view->getVariable("newTable");
+?>
+
+<table>
+        <tr class="topTable">
+            <th><?= i18n("Ejercicios Tabla")?></th>
+            <th><?= i18n("Descripcion")?></th>
+            <th><?= i18n("Grupo_muscular")?></th>
+            <th><?= i18n("Maquina")?></th>
+        </tr>
+        <?php
+          if($exercises != null){
+            foreach($exercisesTable as $exercise):
+           ?>
+            <tr>
+              <th><?= $exercise["nombre"] ?></th>
+              <th><?= $exercise["descripcion"] ?></th>
+              <th><?= $exercise["grupo_muscular"] ?></th>
+              <th><?= $exercise["maquina"] ?></th>
+            </tr>
+            <?php endforeach;
+        }?>
+        <tr>
+          <form action="?controller=session&amp;action=newSession" method="POST">
+              <input name="tableId" value="<?= $tables->getId() ?>" hidden="true">
+              <button><?= i18n("Register Session")?></button></form>
+        </tr>
+</table>
