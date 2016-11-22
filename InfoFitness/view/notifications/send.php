833af5358f18e0317f675f241a678b8877816661
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-22 19:15:59 +0100

    cambios notificaciones

---
+++ InfoFitness/view/notifications/send.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once(__DIR__."/../../core/ViewManager.php");
+$view = ViewManager::getInstance();
+
+
+?>
+
+<form action="index.php?controller=notifications&amp;action=send" method="POST">
+  <?= i18n("To")?>: <input type="email" name="email"></br>
+
+  <!--<input type="radio" name="Departamento" value="informacion@empresa.com" id="Departamento_0"  /> Informacion-->
+  <!--<input type="radio" name="Departamento" value="compras@empresa.com" id="Departamento_1" /> Compras-->
+  <?= i18n("Subject")?>: <input type="text" name="subject"></br>
+  <?= i18n("Message")?>: <input type="textarea" name="message" cols="32" rows="32"></br>
+
+  <input type="submit" value=<?= i18n("Send") ?>>
+</form>
