adab538030bf558f5195fe4f83a6a6dea276f482
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-22 12:51:33 +0100

    view send_notifications creada

---
+++ InfoFitness/view/notifications/send_notifications.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once(__DIR__."/../../core/ViewManager.php");
+$view = ViewManager::getInstance();
+
+
+?>
+
+<form action="index.php?controller=notifications&amp;action=enviar" method="POST">
+  <?= i18n("To")?>: <input type="email" name="email">
+  <?= i18n("Subject")?>: <input type="text" name="subject">
+  <?= i18n("Message")?>: <input type="textarea" name="mensaje">
+
+  <input type="submit" value="<?= i18n("Send") ?>">
+</form>
