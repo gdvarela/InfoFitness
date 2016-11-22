a12a883a93b6f2dcbb2275eb9efc4409249b334a
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-22 19:17:15 +0100

    modificado controller notificacion

--- InfoFitness/controller/NotificationsController.php
+++ InfoFitness/controller/NotificationsController.php
@@ -2,6 +2,10 @@
 
 require_once(__DIR__."/../core/ViewManager.php");
 require_once(__DIR__."/../core/I18n.php");
+require_once(__DIR__ . "/../model/Notification.php");
+require_once(__DIR__ . "/../model/NotificationMapper.php");
+
+require_once(__DIR__ . "/../controller/BaseController.php");
 
 class NotificationsController extends BaseController{
 
@@ -9,12 +13,20 @@ class NotificationsController extends BaseController{
   public function send(){
 
     if(isset($_POST["email"]) && !empty($_POST["email"])
-    && isset($_POST["subject"]) && !empty($_POST["subject"]
-    && isset($_POST["mensaje"]) && !empty($_POST["mensaje"]){
+    && isset($_POST["subject"]) && !empty($_POST["subject"])
+    && isset($_POST["message"]) && !empty($_POST["message"])){
 
-      mail($_POST["email"], $_POST["subject"],$_POST["mensaje"]);
+      $email = $_POST["email"];
+      $subject = $_POST["subject"];
+      $message = $_POST["message"];
+      $from = "FROM: InfoFitness App. Don't reply";
+      mail( $email, $subject , $message, $from);
+
+
+      //$this->view->setFlash("Enviado!");
     }
+    $this->view->render("notifications","send"); //nombre del controlador y nombre de la vista
   }
-
 }
+
 ?>
