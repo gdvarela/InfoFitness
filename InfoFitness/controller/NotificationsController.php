10c3e07bc44cdb4becd4c4a41b5bca68ea7bc73e
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-22 12:41:28 +0100

    NotificationsController añadido

---
+++ InfoFitness/controller/NotificationsController.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(__DIR__."/../core/ViewManager.php");
+require_once(__DIR__."/../core/I18n.php");
+
+class NotificationController extends BaseController{
+
+  //modificar!!!!!! y comprobar
+  public function enviar(){
+
+    if(isset($_POST["email"]) && !empty($_POST["email"])
+    && isset($_POST["subject"]) && !empty($_POST["subject"]
+    && isset($_POST["mensaje"]) && !empty($_POST["mensaje"]){
+
+      mail($_POST["email"], $_POST["subject"],$_POST["mensaje"]);
+    }
+  }
+
+}
+?>
