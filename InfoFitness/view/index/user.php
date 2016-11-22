354b2b654756a49d4440375ee3d9356b83de22a4
lrcortizo <luisraco95@gmail.com>
2016-11-22 18:22:44 +0100

    Cursores botones login, cursor menú, vista principal users

--- InfoFitness/view/index/user.php
+++ InfoFitness/view/index/user.php
@@ -3,7 +3,26 @@ require_once(__DIR__."/../../core/ViewManager.php");
 $view = ViewManager::getInstance();
 
 ?>
-<a href="?controller=activities&action=slotsControl"><?= i18n("Slots Control") ?></a></br>
-<a href="?controller=tables&action=listWorkouts"><?= i18n("Workout Monitoring") ?></a></br>
-<a href="?controller=session&action=listSessions"><?= i18n("Session History") ?></a></br>
-<a href="?controller=exercises&action=listExercises"><?= i18n("Exercises List") ?></a></br>
+
+<div id="user">
+  <a class="linkgeneral" href="?controller=activities&action=slotsControl">
+    <div class="assistance">
+    </br></br><?= i18n("Slots Control") ?>
+  </div>
+ </a>
+ <a class="linkgeneral" href="?controller=tables&action=listWorkouts">
+  <div class="workout">
+    </br></br><?= i18n("Workout Monitoring") ?>
+  </div>
+ </a>
+ <a class="linkgeneral" href="?controller=session&action=listSessions">
+    <div class="session">
+    </br></br><?= i18n("Session History") ?>
+  </div>
+ </a>
+ <a class="linkgeneral" href="?controller=exercises&action=listExercises">
+  <div class="exercise">
+    </br></br><?= i18n("Exercises List") ?>
+  </div>
+ </a>
+</div>
