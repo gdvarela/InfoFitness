506b7812d0326a3f6a61286215b33f8eacea3fe9
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-23 19:57:29 +0100

    descomentada la linea de STMPSecure en notif

--- InfoFitness/controller/NotificationsController.php
+++ InfoFitness/controller/NotificationsController.php
@@ -37,7 +37,7 @@ class NotificationsController extends BaseController{
       //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
       $mail->Port =587;
       //Set the encryption system to use - ssl (deprecated) or tls
-    //$mail->SMTPSecure = 'tls';
+      $mail->SMTPSecure = 'tls';
       //Whether to use SMTP authentication
       $mail->SMTPAuth = true;
       //Username to use for SMTP authentication - use full email address for gmail
