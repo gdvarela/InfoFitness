a78eba9f3116e88280187a79253cd9be93228bb7
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-23 20:31:22 +0100

    vista comentada en proceso....

--- InfoFitness/view/notifications/send.php
+++ InfoFitness/view/notifications/send.php
@@ -9,10 +9,11 @@ $view = ViewManager::getInstance();
 <form action="index.php?controller=notifications&amp;action=send" method="POST">
   <?= i18n("To")?>: <input type="email" name="email"></br>
 
-  <!--<input type="radio" name="Departamento" value="informacion@empresa.com" id="Departamento_0"  /> Informacion-->
-  <!--<input type="radio" name="Departamento" value="compras@empresa.com" id="Departamento_1" /> Compras-->
-  <?= i18n("Subject")?>: <input type="text" name="subject"></br>
-  <?= i18n("Message")?>: <textarea type="textarea" name="message" cols="32" rows="32"></textarea></br>
+<!--  <?= i18n("All")?><input type="radio" name=all value="" />
+  <?= i18n("Athlete")?><input type="radio" name=deportista value="" />
+  <?= i18n("Coach")?><input type="radio" name=monitores value="" /></br></br> -->
+  <?= i18n("Subject")?>: <input type="text" name="subject"></br></br>
+  <?= i18n("Message")?>:</br> <textarea type="textarea" name="message" cols="40" rows="10"></textarea></br>
 
   <input type="submit" value=<?= i18n("Send") ?>>
 </form>
