59438a596db30e3b30d32ba55706cf4d5d53fdc3
lrcortizo <luisraco95@gmail.com>
2016-11-24 18:59:19 +0100

    main view monitor text center

--- InfoFitness/view/index/monitor.php
+++ InfoFitness/view/index/monitor.php
@@ -7,17 +7,17 @@ $view = ViewManager::getInstance();
 <div id=monitor>
   <a href="?controller=exercises&action=listExercises">
     <div class="mainexercises">
-      <?= i18n("Exercises Management") ?>
+    </br></br><?= i18n("Exercises Management") ?>
     </div>
   </a>
   <a href="?controller=tables&action=listTables">
     <div class="maintables">
-      <?= i18n("Tables Management") ?>
+      </br></br><?= i18n("Tables Management") ?>
     </div>
   </a>
   <a href="?controller=activities&action=assistanceControl">
     <div class="mainassistance">
-      <?= i18n("Assistance Control") ?>
+      </br></br><?= i18n("Assistance Control") ?>
     </div>
   </a>
 </div>
