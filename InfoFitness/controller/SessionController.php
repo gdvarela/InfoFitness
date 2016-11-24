82a9abf2412584c43c602eed89198ff7d2fea792
gdvarela <guillermo.davilavarela@gmail.com>
2016-11-24 16:07:50 +0100

    Fix Sessions checkOut

--- InfoFitness/controller/SessionController.php
+++ InfoFitness/controller/SessionController.php
@@ -1,36 +1,51 @@
 <?php
 
- require_once(__DIR__."/../core/ViewManager.php");
- require_once(__DIR__."/../core/I18n.php");
+require_once(__DIR__ . "/../core/ViewManager.php");
+require_once(__DIR__ . "/../core/I18n.php");
 
- require_once(__DIR__."/../model/Session.php");
- require_once(__DIR__."/../model/SessionMapper.php");
+require_once(__DIR__ . "/../model/Session.php");
+require_once(__DIR__ . "/../model/SessionMapper.php");
 
- require_once(__DIR__."/../controller/BaseController.php");
+require_once(__DIR__ . "/../controller/BaseController.php");
 
- class SessionController extends BaseController {
+class SessionController extends BaseController
+{
 
-  private $sessionMapper;
-  private $newSession;
+    private $sessionMapper;
+    private $newSession;
 
-   public function __construct() {
-       parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-       $this->sessionMapper = new SessionMapper();
-       $this->newSession = new Session();
-       // Users controller operates in a "welcome" layout
-       // different to the "default" layout where the internal
-       // menu is displayed
-       $this->view->setLayout("default");
-   }
+        $this->sessionMapper = new SessionMapper();
+        $this->newSession = new Session();
+        // Users controller operates in a "welcome" layout
+        // different to the "default" layout where the internal
+        // menu is displayed
+        $this->view->setLayout("default");
+    }
 
-   public function listSessions(){
-     $sessions = $this->sessionMapper->listSessions();
-     $this->view->setVariable("sessions", $sessions);
-       $this->view->setVariable("title", "Sessions");
-     $this->view->render("session", "list");
-   }
- }
+    public function listSessions()
+    {
+        $sessions = $this->sessionMapper->listSessions();
+        $this->view->setVariable("sessions", $sessions);
+        $this->view->setVariable("title", "Sessions");
+        $this->view->render("session", "list");
+    }
+
+    public function newSession()
+    {
+        if (isset($_POST["tableId"])) {
+            $date = date("Y-m-d H:m", time());
+            $idUser = $_SESSION["userId"];
+            $this->newSession->changeSession($date, $idUser, $_POST["anotacion"], NULL, $_POST["tableId"]);
+            $this->sessionMapper->save($this->newSession);
+            $this->view->redirect("session", "listSessions");
+        }
+    }
+
+}
 
 
- ?>
+?>
--- InfoFitness/model/Session.php
+++ InfoFitness/model/Session.php
@@ -1,43 +1,64 @@
 <?php
 
- require_once(__DIR__."/../core/ValidationException.php");
+require_once(__DIR__ . "/../core/ValidationException.php");
 
- class Session{
-  private $id;
-  private $fecha;
-  private $idUsuario;
-  private $comentario;
-  private $idTabla;
+class Session
+{
+    private $id;
+    private $fecha;
+    private $idUsuario;
+    private $comentario;
+    private $descripcion;
+    private $idTabla;
 
-  public function __construct($id=NULL, $fecha=NULL, $idUsuario=NULL, $comentario=NULL, $descripcion=NULL){
-    $this->id = $id;
-    $this->fecha = $fecha;
-    $this->idUsuario = $idUsuario;
-    $this->comentario = $comentario;
-    $this->descripcion = $descripcion;
-  }
+    public function __construct($id = NULL, $fecha = NULL, $idUsuario = NULL, $comentario = NULL, $descripcion = NULL, $idTabla=NULL)
+    {
+        $this->id = $id;
+        $this->fecha = $fecha;
+        $this->idUsuario = $idUsuario;
+        $this->comentario = $comentario;
+        $this->descripcion = $descripcion;
+        $this->idTabla = $idTabla;
+    }
 
-  public function getId()
-  {
-      return $this->id;
-  }
+    public function getId()
+    {
+        return $this->id;
+    }
 
-  public function getUsuario()
-  {
-      return $this->idUsuario;
-  }
-  public function getFecha()
-  {
-      return $this->fecha;
-  }
-  public function getComentario()
-  {
-      return $this->comentario;
-  }
+    public function getUsuario()
+    {
+        return $this->idUsuario;
+    }
 
-  public function getDescripcion()
-  {
-      return $this->descripcion;
-  }
- }
- ?>
+    public function getFecha()
+    {
+        return $this->fecha;
+    }
+
+    public function getComentario()
+    {
+        return $this->comentario;
+    }
+
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    public function getTabla()
+    {
+        return $this->idTabla;
+    }
+
+    public function changeSession($fecha = NULL, $idUsuario = NULL, $comentario = NULL, $descripcion = NULL, $idTabla=NULL)
+    {
+        $this->fecha = $fecha;
+        $this->idUsuario = $idUsuario;
+        $this->comentario = $comentario;
+        $this->descripcion = $descripcion;
+        $this->idTabla = $idTabla;
+    }
+}
+
+?>
