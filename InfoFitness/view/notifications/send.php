6d41d213967175cb99ea597e2d60534c17b84d88
cgdelgado <cgdelgado@esei.uvigo.es>
2016-11-24 14:01:26 +0100

    seleccion grupo emails

--- InfoFitness/view/notifications/send.php
+++ InfoFitness/view/notifications/send.php
@@ -7,11 +7,11 @@ $view = ViewManager::getInstance();
 ?>
 
 <form action="index.php?controller=notifications&amp;action=send" method="POST">
-  <?= i18n("To")?>: <input type="email" name="email"></br>
+  <?= i18n("To")?>: <!--<input type="email" name="email">--></br>
 
-<!--  <?= i18n("All")?><input type="radio" name=all value="" />
-  <?= i18n("Athlete")?><input type="radio" name=deportista value="" />
-  <?= i18n("Coach")?><input type="radio" name=monitores value="" /></br></br> -->
+  <?= i18n("All")?><input type="radio" name="grupoemail" value="all" />
+  <?= i18n("Athlete")?><input type="radio" name="grupoemail" value="deportistas" />
+  <?= i18n("Coach")?><input type="radio" name="grupoemail" value="monitores" /></br></br>
   <?= i18n("Subject")?>: <input type="text" name="subject"></br></br>
   <?= i18n("Message")?>:</br> <textarea type="textarea" name="message" cols="40" rows="10"></textarea></br>
 
