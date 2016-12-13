bddb4dc60b60763b20b35d164ecc2510bcd0aa3e
cgdelgado <cgdelgado@esei.uvigo.es>
2016-12-13 17:17:07 +0100

    menu perfil en vista index user

--- InfoFitness/view/index/user.php
+++ InfoFitness/view/index/user.php
@@ -19,6 +19,11 @@ $view = ViewManager::getInstance();
     </br></br><?= i18n("Session History") ?>
   </div>
  </a>
+ <a class="linkgeneral" href="?controller=profile&action=modify">
+  <div class="mainexercisespublic">
+  </br></br><?= i18n("Profile") ?>
+  </div>
+ </a>
 <!-- <a class="linkgeneral" href="?controller=exercises&action=listExercises">-->
 <!--  <div class="mainexercisespublic">-->
 <!--    </br></br>--><?//= i18n("Exercises List") ?>
