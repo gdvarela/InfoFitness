558316f927dffbba6194e9b5845f732e8aec4c61
jfsantiago2 <joseangelferro94@gmail.com>
2016-12-17 14:39:16 +0100

    Añadir nueva BD.

--- InfoFitness/InfoFitness.sql
+++ InfoFitness/InfoFitness.sql
@@ -221,7 +221,57 @@ CREATE TABLE `Sesion` (
 
 LOCK TABLES `Sesion` WRITE;
 /*!40000 ALTER TABLE `Sesion` DISABLE KEYS */;
-INSERT INTO `Sesion` VALUES (7,'2016-12-25 00:00:00',1,'Muy duro',1,NULL),(8,'2016-12-31 00:00:00',6,'Muy suave',2,NULL),(9,'2016-11-21 00:00:00',2,'Normalita',NULL,1),(10,'2016-11-23 00:00:00',1,'Aerodance',NULL,13),(11,'2016-11-23 17:11:00',1,'Aerodance',NULL,13);
+INSERT INTO `Sesion` (`id_sesion`, `fecha`, `id_usuario`, `comentario`, `id_tabla`, `id_actividad`) VALUES
+(7, '2016-12-25 00:00:00', 7, 'Muy duro', 1, 13),
+(8, '2016-12-31 00:00:00', 7, 'Muy suave', 2, 17),
+(9, '2016-11-21 00:00:00', 13, 'Normalita', NULL, 1),
+(10, '2016-11-25 16:11:00', 1, 'Aerodance', NULL, 13),
+(11, '2016-11-25 16:11:00', 2, 'Circuit Fit', NULL, 14),
+(13, '2016-11-27 15:11:00', 11, 'Completada', 1, NULL),
+(14, '2016-11-27 16:11:00', 1, 'Demasiado exigente', 1, NULL),
+(15, '2016-11-27 16:11:00', 7, 'Completada', 1, NULL),
+(16, '2016-11-27 17:11:00', 1, 'Spinning', NULL, 1),
+(17, '2016-11-27 17:11:00', 7, 'Spinning', NULL, 1),
+(18, '2016-11-27 17:11:00', 14, 'Spinning', NULL, 1),
+(19, '2016-11-27 17:11:00', 1, 'Aerodance', NULL, 13),
+(20, '2016-11-27 17:11:00', 22, 'Aerodance', NULL, 13),
+(21, '2016-11-27 17:11:00', 1, 'Circuit Fit', NULL, 14),
+(22, '2016-11-27 17:11:00', 2, 'Circuit Fit', NULL, 14),
+(23, '2016-11-27 17:11:00', 6, 'Circuit Fit', NULL, 14),
+(24, '2016-11-27 17:11:00', 7, 'Circuit Fit', NULL, 14),
+(25, '2016-11-27 17:11:00', 11, 'Circuit Fit', NULL, 14),
+(26, '2016-11-27 17:11:00', 2, 'Pilates', NULL, 15),
+(27, '2016-11-27 17:11:00', 6, 'Pilates', NULL, 15),
+(28, '2016-11-27 17:11:00', 7, 'Pilates', NULL, 15),
+(29, '2016-11-27 17:11:00', 13, 'Pilates', NULL, 15),
+(30, '2016-11-27 17:11:00', 6, 'Tai Chi', NULL, 16),
+(31, '2016-11-27 17:11:00', 7, 'Tai Chi', NULL, 16),
+(33, '2016-11-27 17:11:00', 6, 'Hipopresivos', NULL, 19),
+(34, '2016-11-27 17:11:00', 1, 'Power dumbell', NULL, 17),
+(35, '2016-11-27 17:11:00', 7, 'Power dumbell', NULL, 17),
+(36, '2016-12-14 16:12:00', 11, 'Spinning', NULL, 1),
+(37, '2016-12-14 16:12:00', 11, 'Spinning', NULL, 1),
+(38, '2016-12-14 16:12:00', 1, 'Spinning', NULL, 1),
+(39, '2016-12-14 16:12:00', 15, 'Spinning', NULL, 1),
+(40, '2016-12-14 16:12:00', 16, 'Spinning', NULL, 1),
+(41, '2016-11-27 17:11:00', 16, 'Circuit Fit', NULL, 14),
+(42, '2016-11-27 17:11:00', 17, 'Circuit Fit', NULL, 14),
+(43, '2016-11-27 17:11:00', 18, 'Circuit Fit', NULL, 14),
+(44, '2016-11-27 17:11:00', 14, 'Pilates', NULL, 15),
+(45, '2016-11-27 17:11:00', 15, 'Pilates', NULL, 15),
+(46, '2016-11-27 17:11:00', 16, 'Pilates', NULL, 15),
+(47, '2016-11-27 17:11:00', 17, 'Pilates', NULL, 15),
+(48, '2016-11-27 17:11:00', 18, 'Pilates', NULL, 15),
+(49, '2016-11-27 17:11:00', 19, 'Pilates', NULL, 15),
+(50, '2016-11-27 17:11:00', 23, 'Tai Chi', NULL, 16),
+(51, '2016-11-27 17:11:00', 22, 'Tai Chi', NULL, 16),
+(52, '2016-11-27 17:11:00', 21, 'Tai Chi', NULL, 16),
+(53, '2016-11-27 17:11:00', 20, 'Tai Chi', NULL, 16),
+(54, '2016-11-27 17:11:00', 6, 'Hipopresivos', NULL, 19),
+(55, '2016-11-27 17:11:00', 23, 'Hipopresivos', NULL, 19),
+(56, '2016-11-27 17:11:00', 22, 'Power dumbell', NULL, 17),
+(57, '2016-11-27 17:11:00', 18, 'Power dumbell', NULL, 17),
+(58, '2016-11-27 17:11:00', 19, 'Power dumbell', NULL, 17);
 /*!40000 ALTER TABLE `Sesion` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -273,7 +323,20 @@ CREATE TABLE `Tabla_Ejercicios_Deportista` (
 
 LOCK TABLES `Tabla_Ejercicios_Deportista` WRITE;
 /*!40000 ALTER TABLE `Tabla_Ejercicios_Deportista` DISABLE KEYS */;
-INSERT INTO `Tabla_Ejercicios_Deportista` VALUES (1,1),(2,2),(1,3),(2,4);
+INSERT INTO `Tabla_Ejercicios_Deportista` VALUES 
+(1, 1),
+(3, 1),
+(2, 2),
+(1, 3),
+(3, 3),
+(4, 3),
+(1, 4),
+(2, 4),
+(4, 4),
+(1, 5),
+(2, 5),
+(3, 5),
+(4, 5);
 /*!40000 ALTER TABLE `Tabla_Ejercicios_Deportista` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -325,7 +388,8 @@ CREATE TABLE `Usuario` (
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `dni_UNIQUE` (`dni`),
   UNIQUE KEY `id_usuario_UNIQUE` (`login`),
-  UNIQUE KEY `mail_UNIQUE` (`mail`)
+  UNIQUE KEY `mail_UNIQUE` (`mail`),
+    `sexo` varchar(50) COLLATE latin1_spanish_ci DEFAULT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -335,10 +399,37 @@ CREATE TABLE `Usuario` (
 
 LOCK TABLES `Usuario` WRITE;
 /*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
-INSERT INTO `Usuario` VALUES (1,'robertoGz','25648375C','Roberto','Gonzalez','rober@gmail.com','1111',0,'643875438','1987-09-17'),(2,'lrcortizo','44490236V','Luis','Raña','luisraco95@gmail.com','1234',0,'627642372','1995-08-22'),(3,'manuelRa','52689487R','Manuel','Ramos','claugd2012@gmail.com','pass',1,'685365985',NULL),(4,'gdavila','53193712W','Guillermo','Davila Varela','gdavila@esei.uvigo.es','admin',2,'638233356','1995-10-26'),(5,'mariaG','6985478T','Maria','Graña','cgdelgado@esei.uvigo.es','pass',1,'698523654',NULL),(6,'pepitoAl','87538458X','Pepe','Alvarez','pepito@hotmail.com','bdsfbsi',0,'642837238','1990-04-26'),(7,'carlosMz','98569383F','Carlos','Martinez','carlitos@gmail.com','ijnfibidv',0,'623923733','1992-10-10'),(9,'admin','12345678A','Admin','Admin','admin@admin.admin','admin',2,'','2016-11-17'),(10,'monitor','11111111A','Moni','Moni','guillermo.davilavarela@gmail.com','monitor',1,NULL,NULL),(11,'user','22222222A','User','User','user@user.user','user',0,'','1996-05-15'),(12,'newuser','52469845E','newuser','newuser','newuser@newuser.com','(+34) ',0,'+34685214523','1995-05-10');
+INSERT INTO `Usuario` (`id_usuario`, `login`, `dni`, `nombre`, `apellidos`, `mail`, `contraseña`, `permisos`, `telefono`, `fecha_nacimiento`, `sexo`) VALUES
+(1, 'robertoGz', '25648375C', 'Roberto', 'Gonzalez', 'rober@gmail.com', 'dshfbfb', 0, '643875438', '1987-09-17', 'hombre'),
+(2, 'lrcortizo', '44490236V', 'Luis', 'Raña', 'luisraco95@gmail.com', '1234', 0, '627642372', '1995-08-22', 'hombre'),
+(3, 'manuelRa', '52689487R', 'Manuel', 'Ramos', 'manuelramos@gmail.com', 'pass', 1, '685365985', NULL, 'hombre'),
+(4, 'gdavila', '53193712W', 'Guillermo', 'Davila Varela', 'guillermo.davilavarela@gmail.com', 'admin', 2, '638233356', '1995-10-26', 'hombre'),
+(5, 'mariaG', '6985478T', 'Maria', 'Graña', 'maria@gmail.com', 'pass', 1, '698523654', NULL, 'mujer'),
+(6, 'pepitoAl', '87538459X', 'Pepe', 'Alvarez', 'pepito@hotmail.com', 'bdsfbsi', 0, '642837238', '1990-04-26', 'hombre'),
+(7, 'carlosMz', '98569383F', 'Carlos', 'Martinez', 'carlitos@gmail.com', 'ijnfibidv', 0, '623923733', '1992-10-10', 'hombre'),
+(9, 'admin', '12345678A', 'Admin', 'Admin', 'admin@admin.admin', 'admin', 2, '', '2016-11-17', 'mujer'),
+(10, 'monitor', '11111111A', 'Moni', 'Moni', 'moni@moni.moni', 'monitor', 1, NULL, NULL, 'hombre'),
+(11, 'user', '22222222A', 'User', 'User', 'user@user.user', 'user', 0, NULL, NULL, 'hombre'),
+(12, 'newuser', '52469845E', 'newuser', 'newuser', 'newuser@newuser.com', '(+34) ', 0, '+34685214523', '1995-05-10', 'hombre'),
+(13, 'albaF', '69654768T', 'Alba', 'Fernández', 'alba@gmail.com', 'pass', 0, '698245574', '1996-11-14', 'mujer'),
+(14, 'carlaH', '55896624M', 'Carla', 'Hernandez', 'carla@gmail.com', 'pass', 0, '632245589', '1994-03-01', 'mujer'),
+(15, 'vanessaF', '44896324L', 'Vanessa', 'Fernández', 'vanessa@gmail.com', 'pass', 0, '666245574', '1991-02-11', 'mujer'),
+(16, 'silviaP', '77896324Y', 'Silvia', 'Parada', 'silvia@gmail.com', 'pass', 0, '631487974', '1993-08-08', 'mujer'),
+(17, 'aldaraV', '48796523G', 'Aldara', 'Vazquez', 'aldara@gmail.com', 'pass', 0, '632599874', '1994-05-21', 'mujer'),
+(18, 'antoniaD', '87475893K', 'Antonia', 'Dominguez', 'antonia@gmail.com', 'pass', 0, '669855574', '1995-05-11', 'mujer'),
+(19, 'paulaC', '44896324T', 'Paula', 'Carbonero', 'paula@gmail.com', 'pass', 0, '632245541', '1992-01-16', 'mujer'),
+(20, 'fatimaP', '11896547P', 'Fatima', 'Pego', 'fatima@gmail.com', 'pass', 0, '632589625', '1991-09-11', 'mujer'),
+(21, 'ariadnaM', '2259324S', 'Ariadna', 'Martinez', 'ariadna@gmail.com', 'pass', 0, '666953258', '1994-11-11', 'mujer'),
+(22, 'estefaniaJ', '33569741O', 'Estefania', 'Jimenez', 'estefania@gmail.com', 'pass', 0, '666645574', '1995-12-21', 'mujer'),
+(23, 'raquelB', '77859812C', 'Raquel', 'Barbeito', 'raquel@gmail.com', 'pass', 0, '6698741471', '1994-10-03', 'mujer'),
+(24, 'lauraC', '77455596W', 'Laura', 'Castillo', 'laura@gmail.com', 'pass', 0, '674297365', '1998-09-09', 'mujer');
 /*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+CREATE USER 'infoFitness'@'localhost' IDENTIFIED BY 'infoFitness';
+GRANT ALL PRIVILEGES ON * . * TO 'infoFitness'@'localhost';
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
